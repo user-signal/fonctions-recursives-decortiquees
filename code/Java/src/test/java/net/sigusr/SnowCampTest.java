134bfc02adf4116e0c07edc82d61280e13dd9bb0
Frédéric Cabestre <frederic.cabestre@sigusr.net>
2021-12-08 08:59:20 +0100

    Add Java tests

--- code/Java/src/test/java/net/sigusr/SnowCampTest.java
+++ code/Java/src/test/java/net/sigusr/SnowCampTest.java
@@ -2,17 +2,19 @@ package net.sigusr;
 
 import org.junit.jupiter.api.Test;
 
+import static net.sigusr.SnowCamp.fact;
+import static net.sigusr.SnowCamp.factTail;
 import static org.junit.jupiter.api.Assertions.*;
 
 class SnowCampTest {
 
     @Test
-    void fact() {
-        assertTrue(true);
+    void factTest() {
+        assertEquals(6, fact(3));
     }
 
     @Test
-    void factTail() {
-        assertTrue(true);
+    void factTailTest() {
+        assertEquals(6, factTail(3, 1));
     }
 }
