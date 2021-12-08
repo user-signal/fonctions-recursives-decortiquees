9d02424b77e19b6a5aa01d59ee35fd3fe4471853
Frédéric Cabestre <frederic.cabestre@sigusr.net>
2021-12-08 17:37:26 +0100

    Add: Rust trampoline

--- code/Java/src/main/java/net/sigusr/SnowCamp.java
+++ code/Java/src/main/java/net/sigusr/SnowCamp.java
@@ -1,6 +1,23 @@
 package net.sigusr;
 
+import java.util.function.Supplier;
+
 public class SnowCamp {
+    
+    public static class Thunk<T> {
+        private Thunk(T value, Supplier<Thunk<T>> thunk) {
+            done = value != null;
+            this.value = value;
+            this.thunk = thunk;
+        }
+
+        public boolean done;
+        public T value;
+        public Supplier<Thunk<T>> thunk;
+
+        public static <T> Thunk<T> done(T value) { return new Thunk<>(value, null); }
+        public static <T> Thunk<T> call(Supplier<Thunk<T>> thunk) { return new Thunk<>(null, thunk); }
+    }
     
     public static int fact(int n) {
         if (n == 0)
@@ -9,6 +26,13 @@ public class SnowCamp {
             return n * fact(n - 1);
     }
 
+    public static Thunk<Integer> factTramp(int n, int acc) {
+        if (n == 0)
+            return Thunk.done(acc);
+        else
+            return Thunk.call(() -> factTramp(n - 1, n * acc));
+    }
+    
     public static int factTail(int n, int acc) {
         if (n == 0)
             return acc;
--- code/Rust/src/lib.rs
+++ code/Rust/src/lib.rs
@@ -1,9 +1,32 @@
-fn fact(n : u32) -> u32 {
-    return if n == 0 { 1 } else { n * fact(n - 1) }  
+use crate::Thunk::{Call, Done};
+
+fn fact(n: u32) -> u32 {
+    return if n == 0 { 1 } else { n * fact(n - 1) };
 }
 
-fn fact_tail(n : u32, acc : u32) -> u32 {
-    return if n == 0 { acc } else { fact_tail(n - 1, n * acc) }  
+enum Thunk<T> {
+    Done(T),
+    Call(Box<dyn FnOnce() -> Thunk<T>>),
+}
+
+impl <T> Thunk<T> {
+    fn trampoline(self) -> T {
+        let mut current = self;
+        loop {
+            match current {
+                Call(f) => current = f(),
+                Done(v) => return v,
+            }
+        }
+    }
+}
+
+fn fact_tram(n: u32, acc: u32) -> Thunk<u32> {
+    return if n == 0 { Done(acc) } else { Call(Box::new(move || fact_tram(n - 1, n * acc))) };
+}
+
+fn fact_tail(n: u32, acc: u32) -> u32 {
+    return if n == 0 { acc } else { fact_tail(n - 1, n * acc) };
 }
 
 #[cfg(test)]
@@ -13,6 +36,11 @@ mod tests {
     #[test]
     fn test_fact() {
         assert_eq!(6, fact(3))
+    }
+
+    #[test]
+    fn test_fact_tramp() {
+        assert_eq!(6, fact_tram(3, 1).trampoline())
     }
 
     #[test]
