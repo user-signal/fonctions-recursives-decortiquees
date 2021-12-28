dd20f01f3dc62489bb8909d17266dcbbb3189345
Frédéric Cabestre <frederic.cabestre@sigusr.net>
2021-12-28 15:39:07 +0100

    Add slides about stack

--- code/DotNet/FSharp/Factorial.fs
+++ code/DotNet/FSharp/Factorial.fs
@@ -1,9 +1,9 @@
 ﻿module FSharp.Factorial
 
 let rec Fact = function
-    | 0 -> 1
-    | n -> n * Fact (n - 1)
+    | 0u -> 1u
+    | (n:uint) -> n * Fact (n - 1u)
     
 let rec FactTailRecursive acc = function
-    | 0 -> acc
-    | n -> FactTailRecursive (n * acc) (n - 1)
+    | 0u -> acc
+    | (n:uint) -> FactTailRecursive (n * acc) (n - 1u)
