06fd6a9601e51305f46868c4394af45582f0baf2
Frédéric Cabestre <frederic.cabestre@sigusr.net>
2021-12-30 14:23:41 +0100

    Refactor trampoline example

--- code/DotNet/CSharp/Factorial.cs
+++ code/DotNet/CSharp/Factorial.cs
@@ -16,11 +16,14 @@ namespace CSharp
             _ => FactTailRecursive(n * acc, n - 1)
         };
 
-        public interface ITailRec<out T> {
-            public T Run() {
+        public interface ITailRec<out T>
+        {
+            public T Run()
+            {
                 var current = this;
                 for (;;)
-                    switch (current) {
+                    switch (current)
+                    {
                         case Return<T> @return: return @return.Value;
                         case Suspend<T> suspend:
                             current = suspend.Thunk();
@@ -29,19 +32,19 @@ namespace CSharp
             }
         }
 
-        public struct Return<T> : ITailRec<T> {
+        public struct Return<T> : ITailRec<T>
+        {
             public T Value { get; init; }
         }
 
-        public struct Suspend<T> : ITailRec<T> {
+        public struct Suspend<T> : ITailRec<T>
+        {
             public Func<ITailRec<T>> Thunk { get; init; }
         }
 
-        public static ITailRec<uint> FactTrampoline(uint acc, uint n) {
-            if (n == 0)
-                return new Return<uint> { Value = acc };
-            else
-                return new Suspend<uint> { Thunk = () => FactTrampoline(n * acc, n - 1) };
-        }
+        public static ITailRec<uint> FactTrampoline(uint acc, uint n) => n switch {
+            0 => new Return<uint> { Value = acc },
+            _ => new Suspend<uint> { Thunk = () => FactTrampoline(n * acc, n - 1) }
+        };
     }
 }
