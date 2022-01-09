72e74b1d2445af890dc7a15c81b5574b2e834b11
Frédéric Cabestre <frederic.cabestre@sigusr.net>
2022-01-09 19:19:33 +0100

    Add more ressources and closure schema elements

--- code/DotNet/CSharp/Curry.cs
+++ code/DotNet/CSharp/Curry.cs
@@ -7,7 +7,10 @@ public static class Curry
     public static void MainCurry(string[] args)
     {
         var add42 = AddCurried(42);
-        add42(19);
+        var add19 = AddCurried(19);
+        
+        var resA = add42(13);
+        var resB = add19(13);
     }
     
     public static int Add(int x, int y) => x + y;
